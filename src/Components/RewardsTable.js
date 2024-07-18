6d84c39e6b9fe9e164f27f35a64b4176d7cf048d
prathmesh <pkusumbe@gmail.com>
2024-07-18 22:14:13 +0530

    css added

--- src/Components/RewardsTable.js
+++ src/Components/RewardsTable.js
@@ -4,17 +4,28 @@ const RewardsTable = (props) => {
 
   useEffect(() => {
 
-    console.log(props,"test");
+    console.log(props, "test");
   }, [props?.monthlyRewards])
   return (
     <>
-      <ul className='rewardTable'>
+      <table className='centerTable'>
+        <tr>
+          <th>Months</th>
+          <th>Points</th>
+        </tr>
+        {Object.keys(props?.monthlyRewards).map((res) => (
+          <tr key={res}>
+            <td>{res}</td>
+            <td>{props?.monthlyRewards[res]}</td>
+          </tr>))}
+      </table>
+      {/* <ul className='rewardTable'>
         {Object.keys(props?.monthlyRewards).map((res) => (
           <li key={res}>
             {res}:<b>{props?.monthlyRewards[res]}</b> points
           </li>
         ))}
-      </ul>
+      </ul> */}
     </>
   )
 }
